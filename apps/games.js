a90606655f51ff7ca57c4fa72c90e9924f01a58e
sformichella <sformichella@gmail.com>
2020-10-05 14:48:29 -0700

    add games array

---
+++ apps/games.js
@@ -0,0 +1,58 @@
+
+const counterStrikeGlobalOffensive = {
+    id: 'csgo',
+    name: 'Counter-Strike: Global Offensive',
+    developer: 'Valve Corporation',
+    image: 'csgo.png',
+    description: 'a competitive first-person shooter game',
+    category: 'fps',
+    price: 15.00,
+    onSale: false,
+}
+
+const amongUs = {
+    id: 'among-us',
+    name: 'Among Us',
+    developer: 'Innersloth',
+    image: 'among-us.jpeg',
+    description: 'a party game in which players try to outsmart each other',
+    category: 'part-game',
+    price: 5.00,
+    onSale: false,
+}
+
+const forTheKing = {
+    id: 'for-the-king',
+    name: 'For the King',
+    developer: 'Iron Oak Games',
+    image: 'for-the-king.jpeg',
+    description: 'an rpg style rogue-like adventure game'
+    category: 'rogue-like',
+    price: 20.00,
+    onSale: false,
+}
+
+const left4Dead2 = {
+    id: 'l4d2',
+    name: 'Left 4 Dead 2',
+    developer: 'Valve Corporation',
+    image: 'left-4-dead-2.jpeg',
+    description: 'a zombie survival game where four players must battle through hordes of undead',
+    category: 'zombie-fps',
+    price: 10.00,
+    onSale: false,
+}
+
+const stardewValley = {
+    id: 'stardew-valley',
+    name: 'Stardew Valley',
+    developer: 'ConcernedApe',
+    image: 'stardew-valley.jpeg',
+    description: 'rpg style game focused on being a farmer and interacting with the people of Pelican Town',
+    category: 'rpg',
+    price: 15.00,
+    onSale: false,
+}
+
+
+export const games = [counterStrikeGlobalOffensive, amongUs, forTheKing, left4Dead2, stardewValley];
